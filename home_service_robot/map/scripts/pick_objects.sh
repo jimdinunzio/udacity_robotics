651732b53c5bd27a328ee65d47d634c93d4172d3
Jim DiNunzio <jim@dinunzio.com>
2020-06-30 00:18:51 -0700

    [home_service_robot] amended submission - fixed shell script files to set environment variables to point to correct world and map and launch rviz with custom config with marker subscription

--- home_service_robot/map/scripts/pick_objects.sh
+++ home_service_robot/map/scripts/pick_objects.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+export TURTLEBOT_GAZEBO_WORLD_FILE=$HOME/workspace/catkin_ws/src/my_robot/world/myWorld.world
+export TURTLEBOT_GAZEBO_MAP_FILE=$HOME/workspace/catkin_ws/src/map/map.yaml
+
 xterm  -e "source /opt/ros/kinetic/setup.bash; roscore" &
 sleep 5
 xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 7
 xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
-xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; rviz -d ~/workspace/catkin_ws/src/map/rvizConfig/config.rviz" &
 sleep 5
 xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; rosrun pick_objects pick_objects_node; ($SHELL)" &
