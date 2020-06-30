14b5122ad8f22a9da88eb92cf548f375b05f1d4a
Jim DiNunzio <jim@dinunzio.com>
2020-06-30 14:54:21 -0700

    [home_service_robot] Amended submission - created new ros packages to store map and rviz config and absolute paths were removed from the shell script files

--- home_service_robot/map/scripts/add_marker.sh
+++ home_service_robot/map/scripts/add_marker.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-export TURTLEBOT_GAZEBO_WORLD_FILE=$HOME/workspace/catkin_ws/src/my_robot/world/myWorld.world
-export TURTLEBOT_GAZEBO_MAP_FILE=$HOME/workspace/catkin_ws/src/map/map.yaml
+export TURTLEBOT_GAZEBO_WORLD_FILE=`rospack find my_robot`/world/myWorld.world
+export TURTLEBOT_GAZEBO_MAP_FILE=`rospack find maps`/map.yaml
 
 xterm  -e "source /opt/ros/kinetic/setup.bash; roscore" &
 sleep 5
-xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 7
-xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
-xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; rviz -d ~/workspace/catkin_ws/src/map/rvizConfig/config.rviz" &
+xterm -e " rviz -d `rospack find rvizConfig`/config.rviz" &
 sleep 5
-xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; rosrun add_markers_sim add_markers_sim_node" &
+xterm -e " rosrun add_markers_sim add_markers_sim_node" &
--- home_service_robot/map/scripts/home_service.sh
+++ home_service_robot/map/scripts/home_service.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-export TURTLEBOT_GAZEBO_WORLD_FILE=$HOME/workspace/catkin_ws/src/my_robot/world/myWorld.world
-export TURTLEBOT_GAZEBO_MAP_FILE=$HOME/workspace/catkin_ws/src/map/map.yaml
+export TURTLEBOT_GAZEBO_WORLD_FILE=`rospack find my_robot`/world/myWorld.world
+export TURTLEBOT_GAZEBO_MAP_FILE=`rospack find maps`/map.yaml
 
 xterm  -e "source /opt/ros/kinetic/setup.bash; roscore" &
 sleep 5
-xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 7
-xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
-xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; rviz -d ~/workspace/catkin_ws/src/map/rvizConfig/config.rviz" &
+xterm -e " rviz -d `rospack find rvizConfig`/config.rviz" &
 sleep 5
-xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; rosrun pick_objects pick_objects_node" &
+xterm -e " rosrun pick_objects pick_objects_node" &
 sleep 5
-xterm -e "source ~/workspace/catkin_ws/devel/setup.bash; rosrun add_markers add_markers_node" &
+xterm -e " rosrun add_markers add_markers_node" &
